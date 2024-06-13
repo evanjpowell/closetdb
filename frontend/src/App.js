8059f5afbba4759bfeff7a352ba3d18bd0f415bb
evanjpowell <evanp2025@gmail.com>
2024-06-13 14:42:27 -0400

    linking db and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import {
   BrowserRouter, Routes, Route
 } from "react-router-dom";
+import Clothes from './pages/Clothes';
+import Add from './pages/Add';
+import Update from './pages/Update';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element/>
+          <Route path="/" element={<Clothes/>}/>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/update" element={<Update/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/pages/Clothes.jsx
+++ frontend/src/pages/Clothes.jsx
@@ -1,8 +1,38 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Clothes = () => {
+
+    const [clothes, setClothes] = useState([])
+
+    useEffect(()=>{
+        const fetchAllClothes = async () =>{
+            try{
+                const res =  await axios.get("http://localhost:8800/clothes");
+                setClothes(res.data);
+            }catch(err){
+                console.log(err);
+            }
+        }
+        fetchAllClothes();
+    }, []);
+
     return (
-        <div>Clothes</div>
+        <div>
+            <h1>Wardrobe</h1>
+            <div className="clothesArray">
+                {clothes.map(piece=>(
+                    <div className="clothingPiece" key={piece.id}>
+                        {piece.picture && <img src={piece.picture} alt={piece.name} />}
+                        <h2>{piece.name}</h2>
+                        <p>{piece.category}</p>
+                    </div>
+                ))}
+            </div>
+            <button>Add new item</button>
+        </div>
     )
 }
 
