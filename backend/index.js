56fa95849a2d98b6779816213b740bdd163d395c
evanjpowell <evanp2025@gmail.com>
2024-06-13 13:43:57 -0400

    Created frontend react app

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,43 @@
-import express from express
+import express from "express"
+import mysql2 from "mysql2"
 
 const app = express()
+
+const db = mysql2.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Marcy&Acadia37",
+    database:"test"
+})
+
+app.use(express.json())
+
+
+app.get("/", (req, res)=>{
+    res.json("hello, this is the backend")
+})
+
+app.get("/clothes", (req, res)=>{
+    const q = "SELECT * FROM clothes"
+    db.query(q, (err, data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/clothes", (req,res)=>{
+    const q = "INSERT INTO books (`name`, `category`, `picture`) VALUES (?)"
+    const values = [
+        req.body.name,
+        req.body.category,
+        req.body.pic
+    ]
+
+    db.query(q, [values], (err, data)=>{
+        if (err) return res.json(err)
+        return res.json("book has been created successfully")
+    })
+})
 
 app.listen(8800, ()=>{
     console.log("connected to backend")
